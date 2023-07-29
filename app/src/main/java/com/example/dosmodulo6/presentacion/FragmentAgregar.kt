b6c5b46c5da969631150514626505fcf5ac08149
Jane Palacios <saraipalaciosjane@gmail.com>
2023-07-28 23:52:09 -0400

    layout casi ok

---
+++ app/src/main/java/com/example/dosmodulo6/presentacion/FragmentAgregar.kt
@@ -0,0 +1,23 @@
+package com.example.dosmodulo6.presentacion
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.dosmodulo6.R
+
+
+class FragmentAgregar : Fragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_agregar, container, false)
+    }
+
+
+}
