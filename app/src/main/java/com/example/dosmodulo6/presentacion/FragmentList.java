1949fcfee71681bf408e6376dc6529471614c257
Jane Palacios <saraipalaciosjane@gmail.com>
2023-07-31 21:34:45 -0400

    recicle view agregado

---
+++ app/src/main/java/com/example/dosmodulo6/presentacion/FragmentList.java
@@ -0,0 +1,41 @@
+package com.example.dosmodulo6.presentacion;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.dosmodulo6.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link FragmentList#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class FragmentList extends Fragment {
+
+
+    public FragmentList() {
+        // Required empty public constructor
+    }
+
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_list, container, false);
+    }
+}
---
+++ app/src/main/java/com/example/dosmodulo6/presentacion/ItemAdapter.kt
@@ -0,0 +1,69 @@
+package com.example.dosmodulo6.presentacion
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.appcompat.view.menu.MenuView.ItemView
+import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.RecyclerView.Adapter
+import com.example.dosmodulo6.data.local.Item
+import com.example.dosmodulo6.databinding.ItemBinding
+
+/* Pasos para crear un recycler view
+[x ] item layout
+ [x] agregar constraints
+ [x] cambiar altura de constraints layout de item layout a wrap content
+ [X] definir ID
+[x ] layout con recycler view
+[X ] asignar layout manager
+[x ] implementar clase Adapter
+[x]  heredar RecyclerView.Adapter
+[x] creaciones de los métodos: onCreateViewHolder, getItemCount y onBindViewHolder
+[x ] crear clase anidada ViewHolder, que hereda de la clase ViewHolder
+[x] Agregar constructor
+[x] crear objeto, data class
+[x] Crear lista
+[x] asignar tamaño de la lista en getItemCount
+[x] definir Binding Class del Item
+[x] cambiar view por el binding
+[x] declarar variable item en onBindViewHolder
+[x] implementar binding en clase viewHolder
+[x] Adapter+ view holder
+[ x] obtener los datos*/
+
+class ItemAdapter : RecyclerView.Adapter<ItemAdapter.ItemViewHolder>(){
+    lateinit var binding : ItemBinding
+    private  val listItem = mutableListOf<Item>()
+
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemViewHolder {
+        binding = ItemBinding.inflate(LayoutInflater.from(parent.context),parent,false)
+        return ItemViewHolder(binding)
+    }
+
+
+
+    override fun getItemCount(): Int {
+        return listItem.size
+    }
+
+    override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {
+        val item = listItem[position]
+        holder.render(item)
+
+    }
+
+
+    class ItemViewHolder(val v: ItemBinding) : RecyclerView.ViewHolder(v.root){
+        fun render(item: Item){
+            v.txNombrecv.text =item.nombre
+            v.txCantidadCv.text= item.cantidad.toString()
+            v.txPrecioCv.text = item.precio.toString()
+            v.txTotalCv.text = (item.precio* item.cantidad).toString()
+        }
+    }
+    private fun ItemViewHolder(): ItemAdapter.ItemViewHolder {
+
+    }
+}
