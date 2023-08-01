28700c77ea94b6378d8e09fbc2be6d192402cc5e
Jane Palacios <saraipalaciosjane@gmail.com>
2023-07-31 20:01:22 -0400

    ultimos cambios no se ve el layout

--- app/src/main/java/com/example/dosmodulo6/presentacion/FragmentAgregar.kt
+++ app/src/main/java/com/example/dosmodulo6/presentacion/FragmentAgregar.kt
@@ -1,22 +1,42 @@
 package com.example.dosmodulo6.presentacion
 
+import android.app.Activity
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.activityViewModels
 import com.example.dosmodulo6.R
+import com.example.dosmodulo6.databinding.FragmentAgregarBinding
 
 
 class FragmentAgregar : Fragment() {
-
+lateinit var binding: FragmentAgregarBinding
+private val itemVM : ItemVM by activityViewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_agregar, container, false)
+        binding = FragmentAgregarBinding.inflate(layoutInflater,container,false)
+
+        initListener()
+
+
+        return binding.root
+    }
+
+    private fun initListener() {
+        binding.btnGuardar.setOnClickListener {
+
+            var nombre= binding.edNombreItem.text.toString()
+            var precio = binding.edPrecio.text.toString().toInt()
+            var cantidad = binding.edCantidad.text.toString().toInt()
+
+            itemVM.insertItem(nombre, precio, cantidad)
+        }
     }
 
 
